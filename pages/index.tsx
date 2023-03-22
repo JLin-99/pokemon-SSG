3fe211146be51ac221c4636fc1f7834cae3388b8
José Lin (Light-it) <jose.lin@lightit.io>
2023-03-21 21:55:24 -0300

    Add NextUI library

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,15 @@
 import { NextPage } from "next";
 
+import { Button } from "@nextui-org/react";
+
 import React from "react";
 
 const HomePage: NextPage = () => {
-  return <h1>Home Page</h1>;
+  return (
+    <>
+      <Button color="gradient">Home Page</Button>
+    </>
+  );
 };
 
 export default HomePage;
